01b0d33984d462355b07835486ffcc24f544ffcb
Jatin Sharma <jatin.sharma@ilogsolution.com>
2024-02-28 11:20:06 +0530

    push

--- src/components/tabs/Form1Tabs.vue
+++ src/components/tabs/Form1Tabs.vue
@@ -1,21 +1,28 @@
 <template>
   <v-card class="w-full">
     <v-tabs v-model="tab" bg-color="primary">
-      <v-tab density="compact" value="one">Details</v-tab>
-      <v-tab density="compact" value="two">Party Information</v-tab>
-      <v-tab density="compact" value="three">Logistics</v-tab>
+      <v-tab density="compact" value="details">Details</v-tab>
+      <v-tab density="compact" value="party_information"
+        >Party Information</v-tab
+      >
+      <v-tab density="compact" value="logistics">Logistics</v-tab>
     </v-tabs>
 
     <v-card-text class="w-full border-l-2 border-r-2 border-b-2 min-h-96">
       <v-window v-model="tab">
-        <v-window-item value="one">
+        <v-window-item value="details">
           <DetailsItem />
           <DetailsTable />
         </v-window-item>
 
-        <v-window-item value="two">Party Information</v-window-item>
+        <v-window-item value="party_information">
+          <v-tabs v-model="tab" bg-color="primary">
+            <v-tab density="compact" value="general">General</v-tab>
+            <v-tab density="compact" value="two">Party Information</v-tab>
+          </v-tabs>
+        </v-window-item>
 
-        <v-window-item value="three">Logistics</v-window-item>
+        <v-window-item value="logistics">Logistics</v-window-item>
       </v-window>
     </v-card-text>
   </v-card>
@@ -23,8 +30,8 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import DetailsTable from "./form1/DetailsTable.vue";
-import DetailsItem from "./form1/DetailsItem.vue";
+import DetailsTable from "./detailsTab/DetailsTable.vue";
+import DetailsItem from "./detailsTab/DetailsItem.vue";
 
 const tab = ref();
 </script>
